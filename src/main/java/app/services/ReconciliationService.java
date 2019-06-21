7e1105cc270c9e83a9077b355a8b755c857f815e
GiladW <giladw@rumble.me>
2019-06-21 12:40:18 +0300

    ignore negative days when comparing

--- src/main/java/app/services/ReconciliationService.java
+++ src/main/java/app/services/ReconciliationService.java
@@ -10,23 +10,23 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class ReconciliationService {
-    public static final long DAYS_THRESHOLD = 14l;
+    public static final long DAYS_THRESHOLD = 25l;
     private PayableRepository payableRepository;
 
     public ReconciliationService(PayableRepository payableRepository) {
         this.payableRepository = payableRepository;
     }
 
-    public List<Payable> getPayables(Payment payment){
+    public List<Payable> getPayables(Payment payment) {
         Collection<Payable> payables = payableRepository.GetAll();
 //        List<Payable> payableSameRefId = payables.stream().filter(payable -> payable.getRef_id().equals(payment.getRef_id())).collect(Collectors.toList());
         List<Payable> payableSameRefId = payables.stream().filter(payable -> payment.getRef_id().contains(payable.getRef_id())).collect(Collectors.toList());
-        List<Payable> payableSameRefIdNotOld = payableSameRefId.stream().filter(payable -> (getDaysDistance(payment, payable)) < DAYS_THRESHOLD).collect(Collectors.toList());
+        List<Payable> payableSameRefIdNotOld = payableSameRefId.stream().filter(payable -> isPaymentInThreshold(payment, payable)).collect(Collectors.toList());
 
         List<Payable> payablebyDate = payables.stream().filter(payable -> {
                     return isPaymentInThreshold(payment, payable);
                 }
-         ).collect(Collectors.toList());
+        ).collect(Collectors.toList());
 
         if (payableSameRefId.size() == 1)
             return payableSameRefId;
@@ -42,14 +42,14 @@ public class ReconciliationService {
 
     private boolean isPaymentInThreshold(Payment payment, Payable payable) {
         long days = getDaysDistance(payment, payable);
-        if (days >0 && days < DAYS_THRESHOLD)
+        if (days > 0 && days < DAYS_THRESHOLD)
             return true;
         else
             return false;
     }
 
     private long getDaysDistance(Payment payment, Payable payable) {
-        long diff  =  payment.getDateAsDate().getTime() - payable.getDateAsDate().getTime();
+        long diff = payment.getDateAsDate().getTime() - payable.getDateAsDate().getTime();
         long result = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
         return result;
     }
