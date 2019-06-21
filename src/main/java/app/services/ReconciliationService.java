c3ec8ad540c122de435f5ccc14b5008013f931f2
GiladW <giladw@rumble.me>
2019-06-21 10:09:53 +0300

    add text input file

--- src/main/java/app/services/ReconciliationService.java
+++ src/main/java/app/services/ReconciliationService.java
@@ -2,11 +2,25 @@ package app.services;
 
 import app.Entities.Payable;
 import app.Entities.Payment;
+import app.repositories.PayableRepository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ReconciliationService {
+    private PayableRepository payableRepository;
+
+    public ReconciliationService(PayableRepository payableRepository) {
+        this.payableRepository = payableRepository;
+    }
+
     public List<Payable> getPayables(Payment payment){
+        Collection<Payable> payables = payableRepository.GetAll();
+        List<Payable> r = payables.stream().filter(payable -> payable.getRef_id() == payment.getRef_id()).collect(Collectors.toList());
+
+
         return null;
     }
 }
